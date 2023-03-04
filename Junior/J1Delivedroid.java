a06f1dfa931fb802a3384076adcde8f9db32c41b
Simra <104401196+SimraKhan05@users.noreply.github.com>
2023-03-04 14:16:15 -0500

    J1 completed

--- Junior/J1Delivedroid.java
+++ Junior/J1Delivedroid.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class J1Delivedroid {
     public static void main(String[] args) {
-
+        Scanner theScanner = new Scanner(System.in);
+        int numPackagesDelivered = theScanner.nextInt();
+        int numObstacleCollisions = theScanner.nextInt();
+        int score = numPackagesDelivered * 50;
+        score = score + (numObstacleCollisions * -10);
+        if (numPackagesDelivered > numObstacleCollisions) {
+            score = score + 500;
+        }
+        System.out.println(score);
     }
 }
